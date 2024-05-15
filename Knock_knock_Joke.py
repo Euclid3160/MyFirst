9c10772029e309f49e809aa051961e9a7872e7cb
Euclid3160 <rhenry3160@gmail.com>
2024-05-15 16:54:29 -0500

    Add files via upload

---
+++ Knock_knock_Joke.py
@@ -0,0 +1,12 @@
+#My first knock knock joke
+print("*Knock*Knock* \n\"Who's there\"")#line one is steve talking, line two is paul talking
+print(" ")#an empty line means were starting back over at steve talking. 
+print("\"It's Seteve.\" \n\"Steve who.\"\n")
+print("\"Darn it paul you know who I am." , end=" ")
+print("Open the door.\"\n\"That's not a good joke.\" \n\n\"What are you talking about?" , end=" ")
+print("Did you change the locks?\" \n\"You got the setup, but there was no punchline.\"")
+print(" ")
+print("\"Please paul," , end=" ")
+print("its raining.\" \n\"Normally a play on words for a pun, or a twist.\"")
+print(" ")
+print("\"I","HATE","LIVING","HERE\"",sep="-")#I know it's not aesthetically pleasing, but I wanted to play with all the things tlaked about in the lesson. 
